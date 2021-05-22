7c7e418338bbed260926d8c981fce6c155459bc0
Jancik82 <JanSlo7126>
2021-05-22 17:00:50 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/jwd/jwdcolours/JwdcoloursApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.jwdcolours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwdcoloursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwdcoloursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/jwd/jwdcolours/JwdcoloursApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.jwdcolours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwdcoloursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
